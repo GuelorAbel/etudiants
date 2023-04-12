ecf9f86a1066e506dc93d5324b9a781d54348e7b
Aby <gueloraby@gmail.com>
2023-04-12 05:36:53 +0100

    AJout de la bibliothèque REACT-HOOK-FORM, implémentation dans les formulaires de création et de modification

--- frontend/src/pages/AddUser.jsx
+++ frontend/src/pages/AddUser.jsx
@@ -1,49 +1,55 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function AddUser() {
   // état, données dynamiques
-  const [values, setValues] = useState({
-    nom: "",
-    email: ""
-  })
+  const { handleSubmit , register } = useForm();
   const navigate = useNavigate();
 
   // ensemble de comportements
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    axios.post('http://localhost:5000/creer', values)
-    .then(res => {
-      console.log(res);
-      navigate("/")
-    })
-    .catch(err => console.log(err));
-  }
+  const onSubmit = (data) => {
+    axios.post("http://localhost:5000/creer", data)
+      .then((res) => {
+        console.log(res);
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
+  };
 
   // rendu navigateur
   return (
     <section className="px-4 md:px-0 py-9">
-        <div className="container mx-auto">
-            <div className="mt-14">
-              <form onSubmit={handleSubmit}>
-                <h2 className="mb-3 text-2xl uppercase">Ajouter un étudiant</h2>
-                <div className="my-3">
-                  <label htmlFor=""></label>
-                  <input type="text" placeholder="Entrer un nom..." className="w-full max-w-lg outline-none border p-1 mt-1" 
-                  onChange={e => setValues({...values, nom: e.target.value})}
-                  />
-                </div>
-                <div className="my-3">
-                  <label htmlFor=""></label>
-                  <input type="email" placeholder="Entrer un email..." className="w-full max-w-lg outline-none border p-1 mt-1" 
-                  onChange={e => setValues({...values, email: e.target.value})}
-                  />
-                </div>
-                <button className="btn-sm max-w-md bg-green-600 text-[#f8fafc] ">Enregistrer</button>
-              </form>
+      <div className="container mx-auto">
+        <div className="mt-14">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <h2 className="mb-3 text-2xl uppercase">Ajouter un étudiant</h2>
+            <div className="my-3">
+              <label htmlFor="nom">Nom:</label>
+              <input
+                type="text"
+                name="nom"
+                placeholder="Entrer un nom..."
+                className="w-full max-w-lg outline-none border p-1 mt-1"
+                {...register("nom", {required: true})}
+              />
             </div>
+            <div className="my-3">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Entrer un email..."
+                className="w-full max-w-lg outline-none border p-1 mt-1"
+                {...register("email", {required: true})}
+              />
+            </div>
+            <button className="btn-sm max-w-md bg-green-600 text-[#f8fafc]">
+              Enregistrer
+            </button>
+          </form>
         </div>
+      </div>
     </section>
   )
 }
--- frontend/src/pages/Update.jsx
+++ frontend/src/pages/Update.jsx
@@ -1,59 +1,59 @@
-import React, { useEffect, useState } from 'react'
+// import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios'
+import { useForm } from 'react-hook-form'
 
 export default function Update() {
   // état, données dynamiques
-  const {id} = useParams();
+  const { id } = useParams();
   const navigate = useNavigate();
-  const [values, setValues] = useState({
-    nom: "",
-    email: ""
-  })
-
+  const { register, handleSubmit, setValue, formState: { errors } } = useForm();
 
   // ensemble de comportements
-  useEffect(()=> {
+  useEffect(() => {
     axios.get(`http://localhost:5000/details/${id}`)
-    .then(res => {
-      console.log(res)
-      setValues({...values, nom: res.data[0].nom, email: res.data[0].email});
-    })
-    .catch(err => console.log(err))
-  }, [])
+      .then(res => {
+        console.log(res)
+        setValue('nom', res.data[0].nom);
+        setValue('email', res.data[0].email);
+      })
+      .catch(err => console.log(err))
+  }, [id, setValue])
 
   // la fonction qui gère la mise à jour de l'étudiant choisit à partir de son id
-  const handleUpdate = (event) => {
-    event.preventDefault();
-    axios.put(`http://localhost:5000/mettre-a-jour/${id}`, values)
-    .then(res => {
-      console.log(res);
-      navigate('/')
-    })
-    .catch(err => console.log(err));
+  const handleUpdate = (data) => {
+    axios.put(`http://localhost:5000/mettre-a-jour/${id}`, data)
+      .then(res => {
+        console.log(res);
+        navigate('/')
+      })
+      .catch(err => console.log(err));
   }
 
   // rendu navigateur
   return (
     <section className="px-4 md:px-0 py-9">
-        <div className="container mx-auto">
-            <div className="mt-14">
-              <form onSubmit={handleUpdate}>
-                <h2 className="mb-3 text-2xl uppercase">Mettre à jour cet étudiant</h2>
-                <div className="my-3">
-                  <label htmlFor=""></label>
-                  <input type="text" placeholder="Entrer un nom..." className="w-full max-w-lg outline-none border p-1 mt-1" 
-                   value={values.nom} onChange={e => setValues({...values, nom: e.target.value})} />
-                </div>
-                <div className="my-3">
-                  <label htmlFor=""></label>
-                  <input type="email" placeholder="Entrer un email..." className="w-full max-w-lg outline-none border p-1 mt-1" 
-                   value={values.email} onChange={e => setValues({...values, email: e.target.value})} />
-                </div>
-                <button className="btn-sm max-w-md bg-green-600 text-[#f8fafc] ">Mettre à jour</button>
-              </form>
+      <div className="container mx-auto">
+        <div className="mt-14">
+          <form onSubmit={handleSubmit(handleUpdate)}>
+            <h2 className="mb-3 text-2xl uppercase">Mettre à jour cet étudiant</h2>
+            <div className="my-3">
+              <label htmlFor="nom">Nom</label>
+              <input type="text" placeholder="Entrer un nom..." className="w-full max-w-lg outline-none border p-1 mt-1"
+                {...register('nom', { required: true })} />
+              {errors.nom && <span>Le nom est obligatoire</span>}
             </div>
+            <div className="my-3">
+              <label htmlFor="email">Email</label>
+              <input type="email" placeholder="Entrer un email..." className="w-full max-w-lg outline-none border p-1 mt-1"
+                {...register('email', { required: true })} />
+              {errors.email && <span>L'email est obligatoire</span>}
+            </div>
+            <button className="btn-sm max-w-md bg-green-600 text-[#f8fafc] ">Mettre à jour</button>
+          </form>
         </div>
+      </div>
     </section>
   )
 }
